78ca5b2c654af98dfa0e5bb5d98cd190c37e204d
Daniel Kehoe <dwk244@gmail.com>
2016-04-27 09:56:01 +0100

    PS alter in appointment change type

--- db/migrate/20160319233755_change_end_time_type.rb
+++ db/migrate/20160319233755_change_end_time_type.rb
@@ -1,5 +1,5 @@
 class ChangeEndTimeType < ActiveRecord::Migration
   def change
-  	change_column :appointments ,:end_time,  :datetime
+  	change_column :appointments ,:end_time, 'datetime USING CAST(end_time AS datetime without time zone)'
   end
 end
